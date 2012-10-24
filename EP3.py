2fef0776a40150b41f480a330b97cc64e459c3cc
Rewasvat <fernando_aluani@hotmail.com>
2012-10-24 15:42:01 -0200

    adicionei o EP3.py, que seria o ponto de partida do programa, se feito em python

---
+++ EP3.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+########################################################
+#
+#  MAC0448 - EP3
+#  Diogo Haruki Kykuta   #USP: 6879613
+#  Fernando Omar Aluani  #USP: 6797226
+#
+#   Esse é o principal arquivo do EP, o "main"
+#   Rode-o para executar o programa.
+#
+########################################################
+import sys
+
+#TODO:
+#  YO HARUKI!  Coloque aqui código que você quer que fique neste arquivo
+
+########################################################
+def Execute(argList):
+    if len(argList) < 1:
+        print "Wrong program call. Use: "
+        print "EP3.py <network_topology_file_name>"
+        return
+
+    argFile = argList[0] # Network Topology File Name
+    
+    #TODO: implemente aqui o código pra executar o programa de fato
+
+    return
+    
+if __name__ == "__main__":
+    Execute( sys.argv[1:] )
